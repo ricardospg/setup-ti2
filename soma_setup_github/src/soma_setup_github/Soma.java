a3243cac33b1b59c81f234c8b099c87bc46e2c10
ricardospg <ricardospgc@gmail.com>
2023-08-24 15:46:54 -0300

    somar dois números

---
+++ soma_setup_github/src/soma_setup_github/Soma.java
@@ -0,0 +1,21 @@
+package soma_setup_github;
+
+import java.util.*;
+
+class Soma {
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String Args[]) {
+		int num1, num2, soma;
+		
+		System.out.println("Digite um numero: ");
+		num1 = sc.nextInt();
+		
+		System.out.println("Digite outro numero: ");
+		num2 = sc.nextInt();
+		
+		soma = num1 + num2;
+		
+		System.out.println("Soma: " + soma);
+	}
+}
